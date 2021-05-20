03468ec9d13027bbe1f3c1bd069e50ba9f1c50c6
JulioAvalos <julioavalos2@msn.com>
2021-05-19 19:26:21 -0600

    extracting dynamic path segment data (dynamic routes)

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage () {
+    const router = useRouter();
+    
+    console.log(router.pathname);
+    console.log(router.query);
+
     return (
         <div>
             <h1>The Porfolio Project Page</h1>
